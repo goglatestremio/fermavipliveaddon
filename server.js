51d62c1f833e2ecf37d25698cbed84077cac3e43
goglatestremio <ironikirurgu@gmail.comgit config --global user.name goglatestremio>
2025-04-24 22:43:42 +0200

    Fixed server listening on 0.0.0.0

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./index");
 
-serveHTTP(addonInterface, { port: 7000 });
+serveHTTP(addonInterface, { 
+  port: process.env.PORT || 7000,  // Set the port dynamically from Render's environment
+  host: '0.0.0.0'  // Listen on all network interfaces (not just localhost)
+});
