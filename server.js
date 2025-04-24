811e3a6915997ab86d68b79983f0cb7ccd0d5cc3
goglatestremio <ironikirurgu@gmail.comgit config --global user.name goglatestremio>
2025-04-24 22:52:24 +0200

    Remove host configuration, use dynamic port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./index");
 
+// Only set the port dynamically from the environment (Render will use the correct interface)
 serveHTTP(addonInterface, { 
-  port: process.env.PORT || 10000,  // Ensure Render's dynamic port is used
-  host: '0.0.0.0'  // Change this line to make it accessible to the public
+  port: process.env.PORT || 10000  // Render will provide the correct dynamic port
 });
